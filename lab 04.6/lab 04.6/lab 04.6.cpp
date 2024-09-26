86cacf6bc304ec1c0cbc612eba5a42293514e86b
Tanichka2006 <tanya4ych@gmail.com>
2024-09-26 21:23:13 +0300

    Add files via upload

---
+++ lab 04.6/lab 04.6/lab 04.6.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    // перший спосіб - while
+    int i, k;
+    double P, E;
+    P = 1;
+    i = 1;
+
+    while (i <= 3) {
+        k = 1;
+        while (k <= 4 - i) {
+            E = (sin(i * k * k) + cos(k * i * i)) / (k * k + i * i);
+            P *= E;
+            k++;
+        }
+        i++;
+    }
+    cout << "P = " << P << endl;
+
+    // другий спосіб - do while
+    P = 1;
+    i = 1;
+    do {
+        k = 1;
+        do {
+            E = (sin(i * k * k) + cos(k * i * i)) / (k * k + i * i);
+            P *= E;
+            k++;
+        } while (k <= 4 - i);
+        i++;
+    } while (i <= 3);
+    cout << "P = " << P << endl;
+
+    // третій спосіб - for
+    P = 1;
+    for (i = 1; i <= 3; i++) {
+        for (k = 1; k <= 4 - i; k++) {
+            E = (sin(i * k * k) + cos(k * i * i)) / (k * k + i * i);
+            P *= E;
+        }
+    }
+    cout << "P = " << P << endl;
+
+    // четвертий спосіб - for зворотний
+    P = 1;
+    for (i = 3; i >= 1; i--) {
+        for (k = 4 - i; k >= 1; k--) {
+            E = (sin(i * k * k) + cos(k * i * i)) / (k * k + i * i);
+            P *= E;
+        }
+    }
+    cout << "P = " << P << endl;
+
+    return 0;
+}
